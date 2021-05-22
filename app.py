3ac33cbf184d2a02056cd602c2f5df57dfddec1f
snehashish090 <snehashish.laskar@sahyadrischool.org>
2021-05-23 00:46:13 +0530

    added files

---
+++ app.py
@@ -0,0 +1,206 @@
+from flask import (Flask, request, render_template, redirect) 
+import os
+import sqlite3
+import json
+
+with open('logged.json','r') as f:
+    log = json.load(f)
+with open('data.json', 'r') as file:
+    data  = json.load(file)
+
+with open('data2.json', 'r') as file:
+    data2  = json.load(file)
+class User:
+    def __init__(self, username, email, password, ips, data):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.ips = ips
+        self.data = data
+    
+    def getUsername(self):
+        return self.username
+
+    def getEmail(self):
+        return self.email
+
+    def getPassword(self):
+        return self.password
+
+    def getIp(self):
+        return self.ips
+
+    def getSite(self):
+        return self.data
+    # def __str__(self):
+    #     return str(self.username, self.email, self.password)
+
+class Password:
+    def __init__(self, site, used, password):
+        self.site = site
+        self.used = used
+        self.password = password
+
+    def getSite(self):
+        return self.site
+    
+    def getUsed(self):
+        return self.used
+
+    def getPassword(self):
+        return self.password
+
+    # def __str__(self):
+    #     return str(self.site, self.used, self.password)
+
+def startup():
+    if  not os.path.exists('data.db'):
+        with open('data.db', 'w') as file:
+            pass
+
+    if not os.path.exists('userdata.db'):
+        with open('userdata.db', 'w') as file2:
+            pass
+
+    
+def addUser(username, password, email):
+    data.append(User(username, email, password, ips = [], data = []).__dict__)
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent =4)
+
+
+
+
+def addSite(username, site, used, password):
+    for i in data:
+        if i['username'] == username:
+            i['data'].append(Password(site, used, password).__dict__)
+
+    with open('data.json', 'w') as file:
+        json.dump(data, file, indent=4)
+        
+# addSite('shiv', 'google 1', 'snehashish.laskar@gmail.com', 'snehashish')
+
+def listUsers():
+    user = []
+    for i in data:
+        user.append(i['username'])
+
+    return user
+
+
+def listSites(username):
+    sites = None
+    for i in data:
+        if i['username'] == username:
+            sites = i['data']
+
+    return sites
+
+def checkUserExists(user):
+    if user not in listUsers():
+        return False
+    return True
+
+def checkSiteExists(site, user):
+    if site not in listSites(user):
+        return False
+    return True
+
+def filterLogin(name, pw):
+    if name == "" or pw == "":
+        return 'Field Error'
+    elif not checkUserExists(name):
+        return 'User not found'
+    for i in data:
+        if i['username'] == name:
+            if i['password'] != pw:
+                return 'Password error'
+            else:
+                return 'redirect'
+def filterSignup(name, pw):
+    if name == "" or pw == "":
+        return 'Field Error'
+    elif checkUserExists(name):
+        return 'User not found'
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def welcome():
+    if request.method == 'POST':
+        if request.remote_addr not in log.keys():
+            return redirect('https://github.com') 
+    return render_template('index.html')
+
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+def home():
+    return 'Hello this is a password Manager Dashboard'
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login_screen():
+    if request.method == 'POST':
+        name = request.form.get('username')
+        pw = request.form.get('password')
+
+        if filterLogin(name, pw) == 'Field Error':
+            return render_template('login.html',show = True,  logmsg = 'Please do not leave any field empty', sigmesg = 'Please do not leave any field empty')    
+
+        elif filterLogin(name,pw) == 'User not found':
+            return render_template('login.html',show = True, logmsg = 'User not found!')    
+
+
+        elif filterLogin(name,pw) == 'Password error':
+            return render_template('login.html', show = True, sigmesg = 'Password incorrect')    
+        elif filterLogin(name, pw) == 'redirect':
+            # if name not in log.keys():
+            #     log[name] = []
+            # log[name].append(request.remote_addr)
+            if request.remote_addr not in log.keys():
+                log[request.remote_addr] = name
+            with open('logged.json', 'w') as f:
+                json.dump(log, f, indent=4)
+            return redirect('https://github.com/')
+
+        else:
+                    
+            return render_template('login.html')
+    else:
+        if request.remote_addr in log.keys():
+                # log[request.remote_addr] = name
+                #    with open('logged.json', 'w') as f:
+                # json.dump(log, f, indent=4)
+                return redirect('https://github.com/')   
+        return render_template('login.html', show = False)
+
+@app.route('/signup', methods = ['GET', 'POST'])
+def sign_up_screen():
+    if request.method == 'POST':
+        name = request.form.get('username')
+        pw = request.form.get('password')
+        email = request.form.get('email')
+
+        if filterSignup(name, pw) == 'Field Error':
+            return render_template('signup.html',show = True,  logmsg = 'Please do not leave any field empty', sigmesg = 'Please do not leave any field empty')    
+
+        elif filterSignup(name,pw) == 'User not found':
+            return render_template('signup.html',show = True, logmsg = 'User Already exists')
+
+        else:
+            addUser(name, pw, email)
+            return redirect('/login')
+                
+    else:
+        if request.remote_addr in log.keys():
+                return redirect('https://github.com/')   
+        return render_template('signup.html', show = False)
+
+
+
+@app.route('/addPw')
+def add_page():
+    return 'Adding pages'
+
+if __name__ == '__main__':
+    app.run(debug=True, host = '192.168.0.102')
